b052fdf7100b52ccdb7a43bee949aa2f8aef7376
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-08-12 14:54:54 -0300

    atualizando exercicio 009

--- curso Python/exercicios/ex009.py
+++ curso Python/exercicios/ex009.py
@@ -3,9 +3,9 @@
 numero = int(input('Digite um número: '))
 
 print(f'A tabuada de multiplicação desse número é: \n{numero*1}\n{numero*2}\n{numero*3}\n{numero*4}\n{numero*5}\n{numero*6}\n{numero*7}\n{numero*8}\n{numero*9}\n{numero*10}')
-
+print('-' * 30)
 print(f'A tabuada de soma desse número é: \n{numero+1}\n{numero+2}\n{numero+3}\n{numero+4}\n{numero+5}\n{numero+6}\n{numero+7}\n{numero+8}\n{numero+9}\n{numero+10}')
-
+print('-' * 30)
 print(f'A tabuada de divisão desse número é: \n{numero/1}\n{numero/2}\n{numero/3}\n{numero/4}\n{numero/5}\n{numero/6}\n{numero/7}\n{numero/8}\n{numero/9}\n{numero/10}')
-
+print('-' * 30)
 print(f'A tabuada de subtração desse número é: \n{numero-1}\n{numero-2}\n{numero-3}\n{numero-4}\n{numero-5}\n{numero-6}\n{numero-7}\n{numero-8}\n{numero-9}\n{numero-10}')
