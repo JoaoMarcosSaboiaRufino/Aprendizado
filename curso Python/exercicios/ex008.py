22ffd8610628d94049726e1d3a4bcee5f2a4f446
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-08-12 14:41:09 -0300

    atualizando o exercicio 008

--- curso Python/exercicios/ex008.py
+++ curso Python/exercicios/ex008.py
@@ -1,7 +1,12 @@
 #Faça um programa que leia um número em metros, e em seguida exibir esse valor convertido para Centimetros e Milimetros
 
 metros = float(input('Quantos metros? '))
-centimetros = metros * 100
-milimetros = metros * 1000
+cm = metros * 100
+mm = metros * 1000
+dm = metros * 10
+dam = metros / 10
+hm = metros / 100
+km = metros / 1000
 
-print('{}m convertido(s) para CM são {:.2f}cm e para MM são {:.2f}mm '.format(metros, centimetros, milimetros))
+print(f' {metros}m para mm, vão ser {mm}mm \n {metros}m para cm, vão ser {cm}cm \n {metros}m para dm, vão ser {dm}dm \n'
+      f' {metros}m para dam, vão ser {dam}dam \n {metros}m para hm, vão ser {hm}hm \n {metros}m para km, vão ser {km}km')
