a7671d85c4f04d509f61c7a53872fcc00d19250d
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-08-20 17:19:33 -0300

    is

---
+++ curso Python/exercicios/ex026.py
@@ -0,0 +1,7 @@
+'''Faça um programa que leia uma frase pelo teclado e mostre:
+*identifique quantos 'R' tem na frase
+*identifique a posição do primeiro 'R'
+*identifique a posição do ultimo 'R'
+'''
+
+#pendente
