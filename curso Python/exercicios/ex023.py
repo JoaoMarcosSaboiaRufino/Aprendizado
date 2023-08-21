5a57b50aaaa66cd1ecc04516f929313cdebb0183
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-08-21 15:54:41 -0300

    update

--- curso Python/exercicios/ex023.py
+++ curso Python/exercicios/ex023.py
@@ -1,14 +1,25 @@
 '''Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados.'''
 
-numero = int(input('Digite um número entre 0 e 9999: '))
+numero = int(input('Digite um número entre 0 e 9999: ')) / 1000
 
-if numero > 9999 :
-    print('Erro, limite de quatro digitos')
-else:
-    convertido = str(numero)
-    print(f"""o número é: {numero}
-    Unidade {convertido[3]}
-    Dezena {convertido[2]}
-    Centena {convertido[1]}
-    Milhar {convertido[0]}""")
+conversao = str(numero).replace('.', '')
 
+print('''Unidade: {}
+Dezena: {}
+Centena: {}
+Milhar: {}'''.format(conversao[3], conversao[2], conversao[1], conversao[0]))
+
+#outra forma de fazer:
+
+numero2 = input('Digite um número entre 0 e 9999: ')
+conversao1 = (str(int(numero2) / 1000)).replace('.', '')
+comprimento = len(conversao1)
+unidade = conversao1[comprimento - 1]
+dezena = conversao1[comprimento - 2]
+centena = conversao1[comprimento - 3]
+milhar = conversao1[comprimento - 4]
+
+print('''unidade: {}
+dezena:  {}
+centena: {}
+milhar: {}'''.format(unidade, dezena, centena, milhar))
