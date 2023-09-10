9486cb3493fdd0c8afa2cd71471fd2f07528c45e
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-09-10 16:00:23 -0300

    Update ex048.py

--- curso Python/exercicios/ex048.py
+++ curso Python/exercicios/ex048.py
@@ -1,1 +1,11 @@
-kk
+'''Faça um programa que calcule a soma entre todos o números impares que são multiplos
+de três e que se encontram no intervalo de 1 até 500'''
+#--------------------------------------------------------------------------------------------
+
+soma = 0
+
+for n in range(1, 501, 2):
+
+    if n % 3 == 0:
+        soma += n
+        print(soma)
