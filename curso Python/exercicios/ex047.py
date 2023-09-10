622de744e02b58c6a6ef857fb2eec32b269c2288
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-09-10 15:03:13 -0300

    .

---
+++ curso Python/exercicios/ex047.py
@@ -0,0 +1,9 @@
+'''Crie um programa que mostre na tela todos os números pares que estão no intervalo
+entre 1 e 50.'''
+#--------------------------------------------------------------------------------------------------
+from time import sleep
+
+
+for n in range(0, 51, 2):
+    print(n)
+    sleep(0.2)
