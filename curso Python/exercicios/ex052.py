71acc37e1253fa527f7375f92f5982e10b14df02
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-09-26 17:39:57 -0300

    Create ex052.py

---
+++ curso Python/exercicios/ex052.py
@@ -0,0 +1,7 @@
+'''Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.'''
+#----------------------------------------------------------------------------------------
+
+nint = int(input('Digite um número inteiro: '))
+
+for c in range(1, 10):
+    print(c)
