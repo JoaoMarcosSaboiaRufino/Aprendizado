696d5b5aa23fbaa7f3b4a883bfffe9e7309e8202
João Marcos S.R <joaomarcosrufino2@gmail.com>
2023-09-26 18:26:44 -0300

    Update ex052.py

--- curso Python/exercicios/ex052.py
+++ curso Python/exercicios/ex052.py
@@ -1,21 +1,34 @@
 '''Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.'''
 #----------------------------------------------------------------------------------------
 
-num = int(input('Digite um número inteiro: '))
+numero = int(input('Digite um número inteiro: '))
 total = 0
 
-for c in range(1, num + 1):
-    if num % c == 0:
-        print('\033[32m ', end=' ')
+cores = ['\033[32m', '\033[31m']
+
+for nums in range(1, numero + 1):
+    if numero % nums == 0:
+        print(cores[0], end='')
         total += 1
     else:
-        print('\033[31m ', end=' ')
-    print('{}'.format(c), end=' ')
+        print(cores[1], end='')
+    print(nums, end=' ')
 
-
-print('\nO número {} foi divisivel {} vezes.'.format(num, total))
+print('\nO número {} foi divisivel {} vezes.'.format(numero, total))
 
 if total == 2:
     print('E por isso ele é um NÚMERO PRIMO!')
 else:
     print('\033[31mE por isso ele NÃO é um NÚMERO PRIMO!')
+
+
+
+
+
+
+
+
+
+
+
+
